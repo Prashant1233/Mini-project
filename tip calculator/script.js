379da7febe62dae4e25f4cf80136e7bf903ef746
Prashant1233 <89354788+Prashant1233@users.noreply.github.com>
2022-01-12 00:51:03 +0530

    Update script.js

--- tip calculator/script.js
+++ tip calculator/script.js
@@ -1,8 +1,8 @@
 function calculation(){
     let tip=document.getElementById("tip").value;
     let total_person=document.getElementById("total_person").value;
-    if((tip) && (total_person)){
     let service=document.getElementById("service").value;
+    if((tip) && (total_person) && (service)){
     let result=Math.floor(((tip * service)/100)/total_person);
     document.getElementById("show").innerHTML=`<div class="showclass">TIP AMOUNT:- Rs. ${result} Each </div>`;
     }else{
